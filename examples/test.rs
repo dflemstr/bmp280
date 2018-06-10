0838b207a2fe4af42fe797cd6423549a8f121738
David Flemström <david.flemstrom@gmail.com>
2018-06-10 22:34:43 +0200

    Account for sensor offset

--- examples/test.rs
+++ examples/test.rs
@@ -5,12 +5,15 @@ extern crate i2cdev;
 use std::thread;
 use std::time;
 
+// Determined from testing your particular chip
+const OFFSET: f32 = -161.0;
+
 fn main() -> Result<(), failure::Error> {
     let dev = i2cdev::linux::LinuxI2CDevice::new("/dev/i2c-1", 0x77)?;
     let mut bmp = bmp280::Bmp280::new(dev, bmp280::CHIP_ID)?;
 
     loop {
-        let temp = bmp.read_temperature()?;
+        let temp = bmp.read_temperature()? + OFFSET;
         println!("Temperature: {}°C", temp);
         thread::sleep(time::Duration::from_secs(1));
     }
